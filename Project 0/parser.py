2447e1d23da9ac07db3f137f483cd3ed721c8401
Federico Melo Barrero <f.melo@uniandes.edu.co>
2022-08-27 12:33:31 -0500

    Sintax errors described

--- Project 0/parser.py
+++ Project 0/parser.py
@@ -8,7 +8,7 @@ def load_program(file_name: str)->None:
     Args:
         file_name (str): name of the file with the program to check
     """
-    program = open(file_name, "r")
+    program = open(file_name)
     program_str = program.read()
     program.close()
 
@@ -19,20 +19,23 @@ def load_program(file_name: str)->None:
 
     
 
-def fail()->None:
+def sintax_error(error: str)->None:
     """Ends sintax check after finding a sintax error.
        Prints False to inform the program is incorrect and stops execution.
     """
+    print("Sintax error: "+error) # TODO: delete line
     print(False)
     quit()
 
 
 def check_sintax(program_str: str)->bool:
     # PROG and GORP
-    if program_str[0:4] == "PROG" and program_str[-4:] == "GORP":
-        program_str = program_str[4:-4].strip()
-    else:
-        fail()
+    if program_str[0:4] != "PROG":
+        sintax_error("expected 'PROG' to begin program definition.")
+    if program_str[-4:] != "GORP":
+        sintax_error("expected 'GORP' to end program definition.")
+    program_str = program_str[4:-4].strip()
+        
     
     # Variable declaration
     if program_str[0:3] == "VAR":
@@ -57,14 +60,14 @@ def check_variable_declaration(program_str: str)->str:
     """
     pos_var_declaration_end = program_str.find(";")
     if pos_var_declaration_end == -1:
-        fail()
+        sintax_error("expected ';' to end variable declaration.")
     
     # Remove VAR and ;
     var_declaration = program_str[3:pos_var_declaration_end].strip()
 
     # Check if there's at least one comma
     if var_declaration.find(",") == -1:
-        fail()
+        sintax_error("expected ',' to separate variables.")
     var_list = var_declaration.split(",")
     for var in var_list:
         check_name(var.strip())
@@ -82,13 +85,13 @@ def check_name(name: str)->None:
     """
     # First character
     if not name[0].isalpha():  # name doesn't begin with letter
-        fail()
+        sintax_error("name '"+name+"' doesn't begin with a letter.")
 
     # Other characters
     if len(name) > 1:
         for char in name[1:]:
             if not (char.isalpha or char.isnumeric):  # at least one isn't alphanumeric
-                fail()
+                sintax_error("name '"+name+"' isn't alphanumeric.")
 
     
 def check_procedure(procedures: list, program_str: str)->tuple:
@@ -105,30 +108,30 @@ def check_procedure(procedures: list, program_str: str)->tuple:
     # Check for CORP. Remove PROC and CORP
     pos_procedure_end = program_str.find("CORP")
     if pos_procedure_end == -1:
-        fail()
+        sintax_error("expected 'CORP' to end procedure definition.")
     procedure = program_str[4:pos_procedure_end].strip()
     program_str = program_str[pos_procedure_end+4:].strip()
 
     # Extract procedure name
     pos_end_procedure_name = procedure.find(" ")
     if pos_end_procedure_name == -1:
-        fail()
+        sintax_error("expected blankspace character after procedure name.")
     procedure_name = procedure[:pos_end_procedure_name]
     check_name(procedure_name.strip())
     procedure = procedure[pos_end_procedure_name:].strip()
 
     # Extract parameters.
     if procedure[0] != "(":
-        fail()
+        sintax_error("expected '(' before list of parameters in procedure '"+procedure_name+"'.")
     end_parenthesis = procedure.find(")")
     if end_parenthesis == -1:
-        fail()
+        sintax_error("expected ')' after list of parameters in procedure '"+procedure_name+"'.")
     elif end_parenthesis == 1 or end_parenthesis == 2:
         # If ) is in second on third position, () or ( ), there's no parameters
         parameter_count = 0
     else:
         if procedure.find(",") == -1:
-            fail()
+            sintax_error()
         parameters_not_split = procedure[1:end_parenthesis].split(",")
         parameters = [p.strip() for p in parameters_not_split]
         for parameter in parameters:
@@ -141,12 +144,12 @@ def check_procedure(procedures: list, program_str: str)->tuple:
     procedures.append((procedure_name, parameter_count))
 
     # Instructions
-    check_instructions_block(instructions_block.strip())
+    check_instructions_block(instructions_block.strip(), procedure_name)
 
     return (procedures, program_str)
 
 
-def check_instructions_block(block: str)->None:
+def check_instructions_block(block: str, procedure_name: str)->None:
     """Checks the sintax for a block of instructions
     'A block of instructions is a sequence of instructions separated 
     by semicolons within curly brackets'.
@@ -155,8 +158,10 @@ def check_instructions_block(block: str)->None:
         block (str): _description_
     """
     # Check curly brackets
-    if block[0] != "{" or block[-1] != "}":
-        fail()
+    if block[0] != "{":
+        sintax_error("Expected '{' before  block of instructions in procedure '"+procedure_name+"'.")
+    if block[-1] != "}":
+        sintax_error("Expected '}' after  block of instructions in procedure '"+procedure_name+"'.")
     block = block[1:-1].strip()
     pass
     # TODO
@@ -164,9 +169,8 @@ def check_instructions_block(block: str)->None:
 ### Main
 
 def main()->None:
-    file_name = "C:/Users/feder/OneDrive/Escritorio/Local/Uniandes/LyM/Project 0/program.txt"
-    # program_name = "C:/Users/feder/OneDrive/Escritorio/Local/Uniandes/LyM/Project 0/p.txt"
+    # file_name = "program.txt"
+    file_name = "wrong_program.txt"
     load_program(file_name)
-
 
 main()
